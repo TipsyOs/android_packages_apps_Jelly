819c85d7fbb03ade6c90fff5552ad3477d5fdaa4
Luca Stefani <luca020400@lineageos.org>
2017-06-28 12:20:55 +0200

    Jelly: Add show/hide keyboard utilities
    Change-Id: Ifef4e05c206cea43f836fc19e7ef007bf10034fc

--- app/src/main/java/org/lineageos/jelly/utils/UiUtils.java
+++ app/src/main/java/org/lineageos/jelly/utils/UiUtils.java
@@ -28,6 +28,8 @@ import android.support.v4.content.ContextCompat;
 import android.support.v4.graphics.ColorUtils;
 import android.support.v7.graphics.Palette;
 import android.util.TypedValue;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 
 import org.lineageos.jelly.R;
 
@@ -85,4 +87,27 @@ public final class UiUtils {
     public static float dpToPx(Resources res, float dp) {
         return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, res.getDisplayMetrics());
     }
+
+    /**
+     * Shows the software keyboard.
+     *
+     * @param view The currently focused {@link View}, which would receive soft keyboard input.
+     */
+    public static void showKeyboard(View view) {
+        InputMethodManager imm = (InputMethodManager) view.getContext().getSystemService(
+                Context.INPUT_METHOD_SERVICE);
+        imm.toggleSoftInputFromWindow(view.getWindowToken(), 0, 0);
+    }
+
+    /**
+     * Hides the keyboard.
+     *
+     * @param view The {@link View} that is currently accepting input.
+     */
+    public static void hideKeyboard(View view) {
+        InputMethodManager imm = (InputMethodManager) view.getContext().getSystemService(
+                Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
 }
